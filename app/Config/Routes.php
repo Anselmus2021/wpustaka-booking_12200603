c59fdae9de2e4fc164553c16dd43c4e0f06423b4
Anselmus2021 <yunusanselmus123@gmail.com>
2021-10-06 23:23:09 +0700

    Tugas Pertemuan 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,6 +35,7 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->get('/hello', 'SelamatDatang::hal_awal');
 $routes->get('/login', 'SelamatDatang::beranda_login');
+$routes->post('/login', 'Login::cekLogin');
 $routes->get('/daftar-member', 'SelamatDatang::daftar_member');
 
 /*
